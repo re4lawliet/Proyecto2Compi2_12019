57de2a6ae744edb8d3fc10b30bbfed1849a8500e
fish <re4lawliet@gmail.com>
2019-03-25 10:51:42 -0600

    primer comit

---
+++ caas/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Claseejemplo from './claseejemplo';
+class App extends Component {
+  render()  {
+    return (
+      <div>
+
+      <Claseejemplo titulo="titulo :D"/>
+
+      </div>
+    );
+  }
+}
+
+export default App;
